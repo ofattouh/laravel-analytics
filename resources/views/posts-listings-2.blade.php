62eea05012bc56ca8b526cc746776220431865fc
Omar Fattouh <ofattouh@gmail.com>
2025-04-21 02:34:47 -0400

    Added Vue Composition API posts component

--- resources/views/posts-listings-2.blade.php
+++ resources/views/posts-listings-2.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Posts Listings - Vue Composition API/Composable') }}
+            {{ __('Posts Listings') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <!-- Display component using Vue 3 Composition API and Composable to show DB Posts -->
+                    <!-- Display component using Vue 3 Composition API and Composable function to show DB Posts -->
                     <posts-index-2></posts-index-2>
                 </div>
             </div>
--- resources/views/posts-listings.blade.php
+++ resources/views/posts-listings.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Posts Listings - Vue basic') }}
+            {{ __('Posts Listings') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <!-- Display Vue component to show DB Posts -->
+                    <!-- Display Vue component using Vue Options API to show DB Posts -->
                     <posts-index></posts-index>
                 </div>
             </div>
